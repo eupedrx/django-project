6d8b6744dde5f6ab6c3912cfb7339453c14f62c8
PedroSaturno <pedro1saturno@gmail.com>
2026-04-27 15:32:13 -0300

    feat: configuração de statics e urls para o frontend

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    # Home frontend
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+
     # Admin
     path('admin/', admin.site.urls),
     
